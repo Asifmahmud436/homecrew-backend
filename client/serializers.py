2edd2189f72a93ca441aea84f8870acea8cb0b7c
Zettex <safaandsafa4@gmail.com>
2024-08-31 01:30:20 +0600

    user update 5

--- client/serializers.py
+++ client/serializers.py
@@ -5,13 +5,13 @@ from .import models
 class Usererializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = '[username,first_name,last_name]'
 
     def update(self, instance, validated_data):
         instance.username = validated_data.get('username', instance.username)
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.email = validated_data.get('email', instance.email)
+        # instance.email = validated_data.get('email', instance.email)
         instance.save()
         return instance
 
