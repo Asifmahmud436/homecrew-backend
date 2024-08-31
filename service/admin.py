214d5a1574b3db633b7f51ac5b3395eb5ec29650
Zettex <safaandsafa4@gmail.com>
2024-08-31 19:20:29 +0600

    user update 12

--- service/admin.py
+++ service/admin.py
@@ -3,8 +3,7 @@ from service.models import Service
 from service.models import Review
 # Register your models here.
 
-class ReviewModel(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('rating',),}
+
 
 admin.site.register(Service)
-admin.site.register(Review,ReviewModel)
+admin.site.register(Review)
