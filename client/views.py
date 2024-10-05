c725237ecbd4c6531e93cd6d5375da835a8ad88f
Zettex <safaandsafa4@gmail.com>
2024-10-05 18:33:00 +0600

    all links pushed

--- client/views.py
+++ client/views.py
@@ -33,7 +33,7 @@ class UserRegistrationApiView(APIView):
             Client.objects.create(user=user, facebook_Id_link='')
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f"https://homecrew-backend.onrender.com/client/active/{uid}/{token}"
+            confirm_link = f"https://homecrew-backend.vercel.app/client/active/{uid}/{token}"
             # confirm_link = f"http://127.0.0.1:8000/client/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
