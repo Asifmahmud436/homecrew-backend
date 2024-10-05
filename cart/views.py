d164455304a0af26a5a6306047f7724c42e5b0be
Zettex <safaandsafa4@gmail.com>
2024-10-05 22:45:43 +0600

    new query added

--- cart/views.py
+++ cart/views.py
@@ -10,7 +10,10 @@ class CartViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         client_id = self.request.query_params.get('client_id')
+        client_username = self.request.query_params.get('client_username')
         if client_id:
             queryset = queryset.filter(client_id=client_id)
+        if client_username:
+            queryset = queryset.filter(client__user__username__icontains=client_username)
         return queryset
     
